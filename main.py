aeb09a4d5676c6fa983fb1a09d05d9087296ac38
ThisIsMyNick <nobel.gautam@gmail.com>
2017-04-24 13:47:07 -0400

    improvements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 import os
+import numpy
 
 meetsReqs = lambda pw: (lambda l: all(len(n) > 0 for n in l))(([c for c in pw if c in string.uppercase],[c for c in pw if c in string.lowercase],[c for c in pw if c in string.digits]))
 
@@ -19,18 +20,17 @@ def isGoodPassword2(pw):
     lowers = len([c for c in pw if c.islower()])
     nums = len([c for c in pw if c.isdigit()])
     symbs = len([c for c in pw if not c.isupper() and not c.islower() and not c.isdigit()])
-    rating = 1
-    if pw in words or pw == 'correcthorsebatterystaple':
+    rating = 0
+    if pw.lower() in words or pw == 'correcthorsebatterystaple':
         return 1
-    #TODO: this is currently wrong.
-    if uppers > 0: rating += uppers/2
-    if lowers > 0: rating += lowers/2
-    if nums > 0: rating += nums/2
-    if symbs > 0: rating += symbs/2
-    rating = min(10,rating)
+    rating += len(pw)*0.7
+    print rating
+    rating -= numpy.std([uppers,lowers,nums,symbs])*0.2
+    print rating
+    rating = int(round(min(10,max(1,rating))))
     return rating
 
 if __name__ == '__main__':
     meaning = ["Garbage", "Very bad", "Very bad", "Very bad", "Very bad", "Bad", "Decent", "Pretty good", "Good", "Very Good"]
     score = isGoodPassword2('OKwatDOuWANT')
-    print "{}: {}".format(score, meaning[int(score-1)])
+    print "{}: {}".format(score, meaning[score-1])
